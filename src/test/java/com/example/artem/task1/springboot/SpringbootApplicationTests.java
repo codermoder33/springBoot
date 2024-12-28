ce3617f66f825ea7e609d2e2a5e60b872736b687
Artem <iwantallofthem@mail.ru>
2024-12-29 00:19:13 +0800

    create empty project

---
+++ src/test/java/com/example/artem/task1/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artem.task1.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
