44002bfebd435d45f3b4088930d1ed9c02c7d32f
Artem <iwantallofthem@mail.ru>
2025-01-16 02:07:14 +0800

    fixed classname

--- src/main/java/com/example/artem/task1/springboot/SpringbootApplication.java
+++ src/main/java/com/example/artem/task1/springboot/SpringbootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringStudentsApplication {
+public class SpringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringStudentsApplication.class,args);
+        SpringApplication.run(SpringbootApplication.class,args);
     }
 
 }
