74afaabef4b0fa87f2a44947c5b8d80835d66fd9
Artem <iwantallofthem@mail.ru>
2025-01-16 21:06:51 +0800

    change class name

--- src/main/java/com/example/artem/task1/springboot/MyTestSpringBootApplication.java
+++ src/main/java/com/example/artem/task1/springboot/MyTestSpringBootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class MyTestSpringBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootApplication.class,args);
+        SpringApplication.run(MyTestSpringBootApplication.class,args);
     }
 
 }
